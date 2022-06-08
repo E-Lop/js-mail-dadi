dad9daaefe75cb9496c8252f49f054843021edd6
E-Lop <edmondolopez@gmail.com>
2022-06-08 14:19:51 +0200

    creato campo input e cattura in variabile JS

--- Mail/JS/script.js
+++ Mail/JS/script.js
@@ -1,1 +1,14 @@
-alert('Ciao');
+/* Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo. */
+
+let userEmail;
+/* bottone di input */
+const userInfoBtn = document.getElementById('user_input_btn');
+
+/* al click salvo la email dell'utente nella costante userEmail */
+userInfoBtn.addEventListener('click', function () {
+  userEmail = document.getElementById('user_email').value;
+  console.log(userEmail);
+});
