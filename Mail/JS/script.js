2ef4c3e60a53d82a6ced52a9b67ce69e47c83347
E-Lop <edmondolopez@gmail.com>
2022-06-08 14:44:07 +0200

    esercizio mail completato

--- Mail/JS/script.js
+++ Mail/JS/script.js
@@ -3,12 +3,41 @@ Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo. */
 
+// dichiarazione variabile email utente
 let userEmail;
-/* bottone di input */
+
+// variabile flag per indicare se l'utente è autorizzato
+let userAuthorized = false;
+
+// array contenente elenco email autorizzate
+const authorizedEmails = [
+  'uno@mail.com',
+  'due@mail.com',
+  'tre@mail.com',
+  'quattro@mail.com',
+  'cinque@mail.com',
+  'sei@mail.com',
+];
+
+// bottone di input
 const userInfoBtn = document.getElementById('user_input_btn');
 
-/* al click salvo la email dell'utente nella costante userEmail */
+// al click salvo la email dell'utente nella costante userEmail
 userInfoBtn.addEventListener('click', function () {
   userEmail = document.getElementById('user_email').value;
-  console.log(userEmail);
+
+  //   ciclo for per scorrere gli elementi della lista
+  for (let i = 0; i < authorizedEmails.length; i++) {
+    // se input dell'utente uguale a elemento array userAuthorized diventa true
+    if (userEmail === authorizedEmails[i]) {
+      userAuthorized = true;
+    }
+  }
+
+  /****** SEZIONE OUTPUT *******/
+  if (userAuthorized === true) {
+    alert('Accesso consentito');
+  } else {
+    alert('Accesso negato');
+  }
 });
